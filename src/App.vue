e736cda77209b892ca919f0eea910d409ae297d4
scenaristeur <scenaristeur@gmail.com>
2020-11-22 11:59:43 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <PopockLogin />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PopockLogin from 'popock-login/dist/popock-login.min.js'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, PopockLogin
   }
 }
 </script>
